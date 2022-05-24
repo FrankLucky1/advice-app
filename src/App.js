4397ecca060b2d3ec40f78b3f6c94bb81f098fd4
FrankLucky1 <82184684+FrankLucky1@users.noreply.github.com>
2022-05-24 18:08:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css'
+
+const App = () => {
+
+    const [advice, setAdvice] = useState("=")
+
+    const fetchAdvice = async()=>{
+        try{
+           const response = await axios.get('https://api.adviceslip.com/advice');
+                const myAdvice = response.data.slip.advice;
+                setAdvice(myAdvice)
+                } catch (error) {
+                    console.log(error)
+                    };
+    } 
+
+    useEffect(()=>{
+        fetchAdvice();
+    },[])
+
+    const HandleClick = ()=>{
+        fetchAdvice();
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <div className='card'>
+            <h1>{advice}</h1>
+            <button onClick={HandleClick}>Next advice</button>    
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
